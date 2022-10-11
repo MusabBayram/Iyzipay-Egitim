b49e52427b18de05ad91f177e7c5c2e82bb19ced
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-10-11 15:39:36 +0300

    Update installments.js

--- src/routes/installments.js
+++ src/routes/installments.js
@@ -9,5 +9,17 @@ import Carts from "../carts";
 const { ObjectId } = Types;
 
 export default (router) => {
-    
+    router.post("/installments", Session, async (req, res) => {
+        const {binNumber, price} = req.body;
+        if(!binNumber || !price){
+            throw new ApiError("Missing Parameters", 400, "missingParameters")
+        }
+        const result = await Installments.checkInstallment({
+            locale: req.user.locale,
+            conversationId: nanoid(),
+            binNumber: binNumber,
+            price: price
+        })
+        res.json(result)
+    })
 }
