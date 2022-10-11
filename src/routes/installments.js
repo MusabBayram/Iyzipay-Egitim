11bbcaff71699ee37462e19b1edec181198de0c9
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-10-11 15:32:58 +0300

    Create installments.js

---
+++ src/routes/installments.js
@@ -0,0 +1,9 @@
+import { Types } from "mongoose";
+import moment from "moment/moment";
+import Session from "../middlewares/Session";
+import nanoid from "../utils/nanoid";
+import * as Installments from "../services/iyzico/methods/installments";
+import ApiError from "../error/ApiError";
+import Carts from "../carts";
+
+const { ObjectId } = Types;
