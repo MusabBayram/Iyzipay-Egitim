d56db77173b7743dc2c338c405fc4c1faa177b91
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-10-12 15:02:22 +0300

    Update installments.js

--- src/routes/installments.js
+++ src/routes/installments.js
@@ -9,6 +9,8 @@ import Carts from "../db/carts";
 const { ObjectId } = Types;
 
 export default (router) => {
+    //Fiyata Göre Taksit Kontrolü
+
     router.post("/installments", Session, async (req, res) => {
         
         const {binNumber, price} = req.body;
@@ -21,6 +23,39 @@ export default (router) => {
             binNumber: binNumber,
             price: price
         })
+        res.json(result)
+    })
+
+    //Sepetin Göre Taksit Kontrolü
+    router.post("/installments/:cartId", Session, async (req, res) => {
+        
+        const {binNumber} = req.body;
+        const {cartId} = req.params;
+
+        if(!cartId){
+            throw new ApiError("Cart id is required", 400, "cartIdRequired")
+        }
+
+        const cart = await Carts.findOne({
+            _id: ObjectId(cartId)
+        }).populate("products", {
+            _id: 1,
+            price: 1
+        })
+
+        const price = cart.products.map((product) => product.price).reduce((a,b) => a+b, 0);
+
+        if(!binNumber || !price){
+            throw new ApiError("Missing Parameters", 400, "missingParameters")
+        }
+
+        const result = await Installments.checkInstallment({
+            locale: req.user.locale,
+            conversationId: nanoid(),
+            binNumber: binNumber,
+            price: price
+        })
+
         res.json(result)
     })
 }
