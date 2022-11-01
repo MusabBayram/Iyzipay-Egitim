677c8bf2ed21be1430b5eeef00ade17fd604e293
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-01 13:11:46 +0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ import Users from "./users";
 import Cards from "./cards"
 import Installments from "./installments";
 import Payments from "./payments";
+import PaymentsThreeDS from "./payments-threeds";
 
 export default [
     Test,
     Users,
     Cards,
     Installments,
-    Payments
+    Payments,
+    PaymentsThreeDS
 ]
