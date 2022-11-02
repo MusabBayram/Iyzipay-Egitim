dbb936eb450b51fb62dbe76be31c1799adf5f3b3
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-02 14:56:35 +0300

    Update cancel-payments.js

--- src/routes/cancel-payments.js
+++ src/routes/cancel-payments.js
@@ -10,5 +10,31 @@ const {ObjectId} = Types;
 const reasonEnum = ["double_payment", "buyer_request", "fraud", "other"];
 
 export default (router) => {
-    
+    //CANCEL WHOLE PAYMENT
+    router.post("/payments/:paymentSuccessId/cancel", Session, async (req, res) => {
+
+        const {reason, description} = req.body;
+        const {paymentSuccessId} = req.params;
+        const reasonObj = {};
+
+        if(!paymentSuccessId){
+            throw new ApiError ("PaymentSuccessId is required", 400, "paymentSuccessIdRequired");
+        }
+        if(reason && description){
+            if(!reasonEnum.includes(reason)){
+                throw new ApiError("Invalid cancel payment reason", 400, "invalidCancelPaymentReason");
+            }
+            reasonObj.reason = reason;
+            reasonObj.description = description;
+        }
+        const payment = await PaymentsSuccess.findOne({_id: ObjectId(paymentSuccessId)});
+        const result = await CamcelPayments.cancelPayments({
+            locale: req.user.locale,
+            conversationId: nanoid(),
+            paymentId: payment?.paymentId,
+            ip: req.user?.ip,
+            ...reasonObj
+        })
+        res.json(result);
+    })
 }
